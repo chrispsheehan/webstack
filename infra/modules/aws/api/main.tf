031e1180c46a237e8322eb2bd333b7d449559886
chrispsheehan <chrispsheehan@gmail.com>
2025-04-07 11:56:36 +0100

    fix: route53 record

--- infra/modules/aws/api/main.tf
+++ infra/modules/aws/api/main.tf
@@ -151,6 +151,18 @@ resource "aws_apigatewayv2_domain_name" "this" {
   }
 }
 
+resource "aws_route53_record" "api" {
+  zone_id = data.aws_route53_zone.this.id
+  name    = aws_apigatewayv2_domain_name.this.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.this.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.this.domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
 resource "aws_apigatewayv2_api" "this" {
   name          = local.lambda_api_name
   protocol_type = "HTTP"
