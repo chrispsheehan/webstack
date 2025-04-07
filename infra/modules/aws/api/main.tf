1793d657e958ed2d52c510de79cf2f3cab471a9c
chrispsheehan <chrispsheehan@gmail.com>
2025-04-07 11:35:52 +0100

    fix: add api mapping

--- infra/modules/aws/api/main.tf
+++ infra/modules/aws/api/main.tf
@@ -186,6 +186,12 @@ resource "aws_apigatewayv2_stage" "this" {
   }
 }
 
+resource "aws_apigatewayv2_api_mapping" "this" {
+  api_id      = aws_apigatewayv2_api.this.id
+  domain_name = aws_apigatewayv2_domain_name.this.id
+  stage       = aws_apigatewayv2_stage.this[0].id
+}
+
 resource "aws_apigatewayv2_authorizer" "this" {
   api_id          = aws_apigatewayv2_api.this.id
   authorizer_type = "REQUEST"
