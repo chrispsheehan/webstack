65347d787eeb3574666710535dce2d600a6f431f
chrispsheehan <chrispsheehan@gmail.com>
2025-04-07 15:18:44 +0100

    fix: fmt

--- infra/modules/aws/security/versions.tf
+++ infra/modules/aws/security/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 1.0.8"
   required_providers {
     aws = {
-      version               = ">= 4.15.0"
-      source                = "hashicorp/aws"
+      version = ">= 4.15.0"
+      source  = "hashicorp/aws"
     }
   }
 }
