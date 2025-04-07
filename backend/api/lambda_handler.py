d09bb69bea844fd157641e8f9b69e22a06cbe0e0
chrispsheehan <chrispsheehan@gmail.com>
2025-04-07 14:27:31 +0100

    fix: rm /dev from lambda

--- backend/api/lambda_handler.py
+++ backend/api/lambda_handler.py
@@ -2,17 +2,17 @@ import json
 
 def handler(event, context):
     # Extract the path from the event
-    path = event['requestContext']['path']
+    path = event['rawPath']
     
     # Set a default response
     response_body = {"message": "Path not found"}
     
     # Handle different paths based on the `path` parameter
-    if path == '/dev/hello':
+    if path == '/hello':
         response_body = {"message": "Hello, World!"}
-    elif path == '/dev/test':
+    elif path == '/test':
         response_body = {"message": "This is the test route"}
-    elif path == '/dev/goodbye':
+    elif path == '/goodbye':
         response_body = {"message": "Goodbye, see you later!"}
     else:
         response_body = {"message": f"Unknown path: {path}"}
