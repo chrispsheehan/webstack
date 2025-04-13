4ff73e59b4f862fcaf7ed7b2d512985e287dd937
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2025-04-13 15:54:51 +0100

    fix: format

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,20 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import image from '@astrojs/image';
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [image()],
-    vite: {
-       server: {
-            proxy: {
-                '/api': {
-                    target: 'http://localhost:8080',
-                    changeOrigin: true,
-                    secure: false,
-                    rewrite: (path) => path.replace(/^\/api/, ''),
-                }
-            }
-        }
-    }
+  integrations: [image()],
+  vite: {
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:8080",
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  },
 });
