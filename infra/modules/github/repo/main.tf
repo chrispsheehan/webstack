9eb06e21e14b5f126020cf3cc0c66ece027212e2
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2025-04-15 20:32:51 +0100

    chore: allow self review

--- infra/modules/github/repo/main.tf
+++ infra/modules/github/repo/main.tf
@@ -20,6 +20,7 @@ resource "github_branch_protection" "main" {
   enforce_admins      = true
 
   required_pull_request_reviews {
+    required_approving_review_count = 0
     dismiss_stale_reviews      = true
     require_code_owner_reviews = false
     require_last_push_approval = false
