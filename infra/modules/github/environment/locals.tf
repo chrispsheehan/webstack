04ecc562967667b9709e8198463869f8350f78cd
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2025-05-11 20:04:41 +0100

    chore: check s3 version on deploy (#58)
    * feat: +just check-version
    
    * chore: inc s3 vars in gh environments
    
    * feat: add check versions to depoy job
    
    * chore: step for check version
    
    * chore: script echo fix
    
    * fix: fmt

--- infra/modules/github/environment/locals.tf
+++ infra/modules/github/environment/locals.tf
@@ -1,7 +1,9 @@
 locals {
   variables = {
     AWS_OIDC_ROLE_ARN = "arn:aws:iam::${var.aws_account_id}:role/${var.deploy_role_name}",
-    AWS_REGION        = var.aws_region
-    BASE_DOMAIN       = var.root_domain
+    AWS_REGION        = var.aws_region,
+    BASE_DOMAIN       = var.root_domain,
+    LAMBDA_S3_BUCKET  = var.lambda_bucket,
+    WEB_S3_BUCKET     = var.web_bucket
   }
 }
