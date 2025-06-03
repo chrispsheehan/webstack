e6863254f66e11e3d4f147a10a4305ece0d2cbed
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2025-06-03 17:22:11 +0100

    chore: move things - no security module (#72)
    * chore: rename module
    
    * chore: mv api key to api module
    
    * chore: use resource
    
    * chore: remove security
    
    * fix: rm data block

--- infra/modules/aws/security/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_name" {
-  description = "Name of project - used in naming"
-  type        = string
-}
-
-variable "api_key_ssm" {
-  description = "Name of ssm param used to store api key"
-  type        = string
-}
