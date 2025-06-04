71a300e9740ca454121814680151972624e076fb
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2025-06-04 19:11:29 +0100

    chore: rm api module

--- infra/modules/aws/api/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  lambda_runtime   = "python3.11"
-  auth_header_name = "X-Custom-Auth-Header"
-  lambda_api_key   = "${var.deploy_version}/${var.api_lambda_zip}"
-  lambda_auth_key  = "${var.deploy_version}/${var.auth_lambda_zip}"
-}
--- infra/modules/aws/api/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "api_invoke_domain" {
-  value = replace(
-    replace(aws_apigatewayv2_stage.this.invoke_url, "https://", ""),
-    "/${aws_apigatewayv2_stage.this.name}",
-    ""
-  )
-}
