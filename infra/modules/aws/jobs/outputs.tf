4dcde004d00b6dcbea80c059496f1369bf987655
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2025-06-07 20:52:39 +0100

    chore: add ci trigger

--- infra/modules/aws/jobs/outputs.tf
+++ infra/modules/aws/jobs/outputs.tf
@@ -1,3 +1,7 @@
 output "lambda_cost_explorer_name" {
   value = aws_lambda_function.cost_explorer.function_name
 }
+
+output "lambda_log_processor_name" {
+  value = aws_lambda_function.log_processor.function_name
+}
