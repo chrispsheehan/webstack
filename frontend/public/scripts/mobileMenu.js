cc830bdd11ebbaf65e47df143a8b5a466751d353
chrispsheehan <chrispsheehan@gmail.com>
2025-06-19 12:25:09 +0100

    chore: move js script code

---
+++ frontend/public/scripts/mobileMenu.js
@@ -0,0 +1,14 @@
+export function toggleMenu() {
+  const overlay = document.getElementById('mobile-overlay');
+  const burger = document.getElementById('burger');
+  overlay?.classList.toggle('show');
+  burger?.classList.toggle('show');
+}
+
+export function hideMenu() {
+  document.getElementById('mobile-overlay')?.classList.remove('show');
+  document.getElementById('burger')?.classList.remove('show');
+}
+
+window.toggleMenu = toggleMenu;
+window.hideMenu = hideMenu;
