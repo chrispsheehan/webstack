d81698785600e78337dac729ac69faf75cf02d8a
chrispsheehan <chrispsheehan@gmail.com>
2026-01-09 15:47:50 +0000

    chore: pass in version to func

--- infra/modules/aws/website/code/append-index-to-paths.js.tpl
+++ infra/modules/aws/website/code/append-index-to-paths.js.tpl
@@ -2,11 +2,13 @@ function handler(event) {
   var request = event.request;
   var uri = request.uri;
 
+  var VERSION = "${version}";
+
   // Treat anything without a dot in the path as an SPA route
   var isAsset = uri.includes(".");
 
   if (!isAsset) {
-    request.uri = "/index.html";
+    request.uri = "/" + VERSION + "/index.html";
   }
 
   return request;
--- infra/modules/aws/website/locals.tf
+++ infra/modules/aws/website/locals.tf
@@ -10,6 +10,12 @@ locals {
   cloudfront_iam_values   = var.initial_deploy ? [local.cloudfront_wildcard_arn] : [try(aws_cloudfront_distribution.this.arn, local.cloudfront_wildcard_arn)]
 
   function_runtime           = "cloudfront-js-1.0"
-  append_index_html_code     = templatefile("${path.module}/code/append-index-to-paths.js.tpl", {})
+  append_index_html_code = templatefile(
+    "${path.module}/code/append-index-to-paths.js.tpl",
+    {
+      version = var.deploy_version
+    }
+  )
+
   function_append_index_html = "${local.reference}-append-index-to-path"
 }
